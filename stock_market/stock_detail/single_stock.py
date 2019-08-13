bb27500b7a39ebc353448d57c2ff74fbf31049b1
Ali Khundmiri <salikhundmiri@gmail.com>
2019-08-13 20:13:47 +0530

    code to find stock details of a single stock

---
+++ stock_market/stock_detail/single_stock.py
@@ -0,0 +1,12 @@
+from bsedata.bse import BSE
+
+b = BSE()
+q = b.getQuote(str(532454))
+# print(q)
+print("company Name 		 : {}".format(q['companyName']))
+print("--------------------------------------------")
+print("current Value 		 : {}".format(q['currentValue']))
+print("52 week High 		 : {}".format(q['52weekHigh']))
+print("52 week Low  	  	 : {}".format(q['52weekLow']))
+print("totat Traded Value 	 : {}".format(q['totalTradedValue']))
+print("total Traded Quantity : {}".format(q['totalTradedQuantity']))
