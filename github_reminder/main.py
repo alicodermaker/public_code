0ec45224e606f0129f62c307fe4f20f2d967acfd
Ali Khundmiri <salikhundmiri@gmail.com>
2019-09-18 20:43:13 +0530

    telegram bot to message me is I havent commited in 4 days

---
+++ github_reminder/main.py
@@ -0,0 +1,52 @@
+import os, sys
+from github import Github, GithubException
+from credentials import user, password, access_token
+import datetime 
+
+sys.path.append(os.path.abspath(os.path.join(os.path.join(os.path.join(os.path.realpath(__file__), '..'), '..'),'..')))
+
+# print(sys.path)
+from public_code.credentials import TELEGRAM_TOKEN_METROREMINDER, PERSONAL_ID_TELEGRAM
+from public_code.send_telegram_notification import send_message
+from public_code.create_log import log_error, log_status
+
+
+def main():
+	
+	# First create a Github instance using an access token
+	g = Github(access_token)
+	user = g.get_user()
+	print(user.login)
+
+	# Then play with your Github objects:
+	last_update_repo = []
+	date_today = datetime.datetime.now()
+	print("Scanning your repos... Please wait")
+
+
+	for repo in g.get_user().get_repos():
+		try:
+			commit = repo.get_commit(sha='master')
+			last_commit = repo.name,commit.commit.committer.date
+			# print("{}".format(type(last_commit[1])))
+			delta = date_today - last_commit[1]
+			last_update_repo.append(delta.days)
+			# print("{} -- Last commit: {} days ago".format(repo.name,delta))
+		except GithubException as e:
+			# print(e)
+			pass
+
+	last_update = min(last_update_repo)
+	print(last_update)
+
+	if last_update > 3:
+		print("More than 3 days since your commit. Notifying Admin.")
+		notify("You havent written commits in {} days".format(last_update), "How are you lived past {} days?".format(last_update), "Long Time No Commit", 'Hero')
+
+def notify(title, text, subtitle, Audio):
+	send_message(title, text, subtitle)
+	os.system("""osascript -e 'display notification "{}" with title "{}" subtitle "{}" sound name "{}"'""".format(title, subtitle, text, Audio))
+
+
+if __name__ == '__main__':
+	main()
